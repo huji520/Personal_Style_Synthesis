9937db4329f414fd51e4a5a5620e0533dd1f40fa
Tal Nitzan <Talnitz2706@gmail.com>
2019-10-24 21:58:10 +0300

    fix axis directions and add ref_pic to plot_crop_image

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 from Analyzer import Analyzer
 from Participant import Participant
-from PIL import Image
-from matplotlib.pyplot import imread, imshow, imsave
-import matplotlib.pyplot as plt
-import numpy as np
 
 
 if __name__ == "__main__":
     input1 = "data/D_01/aliza/aliza__130319_0935_D_01.txt"
     # input2 = "data/D_01/zoey/zoey__130319_1208_D_01.txt"
-    # input3 = "data/F_05/aliza/aliza__040619_1842_F_05.txt"
+    input3 = "data/F_05/aliza/aliza__040619_1842_F_05.txt"
     draw = Analyzer.create_drawing(input1)
-    draw.plot_crop_image()
     # draw.speed_vs_time(pause=True)
     # draw.length_vs_time()
     # draw.pressure_vs_time()
     # draw.plot_picture()
+    draw.plot_crop_image()
 
     # person = Participant("aliza")
     # person.plot_participant_pictures()
