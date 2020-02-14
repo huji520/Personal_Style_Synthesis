9f39f8fb450770fda4be1229364b431a18fc701d
shahar.jacob <shahar.jacob@cs.huji.ac.il>
2020-02-14 09:31:33 +0200

    simplify clusters

--- simplify_cluster.py
+++ simplify_cluster.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 def distance(pt_1, pt_2):
     pt_1 = np.array((pt_1[0], pt_1[1]))
@@ -15,29 +16,42 @@ def closest_points(start_point, points, dist):
     return pt
 
 
+def add_next_point(result, curve):
+    rmv = []
+    next_point = [0, 0]
+    lenP = len(result)
+    for i in range(lenP):
+        next_point[0] += result[i][0]
+        next_point[1] += result[i][1]
+        rmv.append(result[i])
+
+    next_point[0] /= lenP
+    next_point[1] /= lenP
+    curve.append(next_point)
+    return rmv
+
+
+def remove_points(points, points_to_remove):
+    for i in range(len(points_to_remove)):
+        j = 0
+        while j < len(points):
+            if points_to_remove[i][0] == points[j][0] and points_to_remove[i][1] == points[j][1]:
+                points = points[:j] + points[j + 1:]
+                j = len(points) - 1
+            j += 1
+    return points
+
+
 def calc_curve(points, dist):
-    curve = []  # list of control points of the current curve we work on
+
     all_curve = []  # list of lists
     pt = points.pop(0)
-
+    curve = []  # list of control points of the current curve we work on
     while len(points) > 0:
         result = closest_points(pt, points, dist)
-        if result:
-            rmv = []
-            next_point = [0, 0]
-            lenP = len(result)
-            for i in range(lenP):
-                next_point[0] += result[i][0]
-                next_point[1] += result[i][1]
-                rmv.append(result[i])
-
-            next_point[0] /= lenP
-            next_point[1] /= lenP
-            curve.append(next_point)
-
-            # remove all the closest points
-            for i in range(lenP):
-                points.remove(rmv[i])
+        if len(result) > 0:
+            points_to_remove = add_next_point(result, curve)
+            points = remove_points(points, points_to_remove)
 
             # if we are done append the end point
             if len(points) == 0:
@@ -45,55 +59,46 @@ def calc_curve(points, dist):
                 all_curve.append(curve)
 
         else:
+            # no close points are found and the curve is still empty
             if len(curve) == 0:
-                print("len(b)==0):")
-                break
+                points = points[1:]
+                if len(points) == 0:
+                    break
+                pt = points.pop(0)
+                continue
 
             curveSartpoint = curve[0]
             result = closest_points(curveSartpoint, points, 2 * dist)
-            if result:
+            if len(result) > 0:
                 curve.reverse()
                 pt = result[0]
                 continue
-
             else:
-                print("no close points found so save b and continue from wherever")
                 all_curve.append(curve)
+                if len(points) == 0:
+                    break
                 pt = points.pop(0)
                 curve = []
                 continue
-
     return all_curve
 
 
-x = [670, 671, 672, 673, 675, 677, 680, 682, 683, 685, 686, 688, 689, 690, 690, 691, 691, 690]
-y = [283, 282, 282, 283, 284, 285, 287, 289, 291, 296, 299, 300, 303, 305, 308, 313, 316, 320]
+def show_simplifican(x, y, i):
+    points = np.stack((x, y), axis=1)
+    points = list(points)
+    dist = 14
+    curves = np.array(calc_curve(points, dist))
+    points = []
+    for curve in curves:
+        for point in curve:
+            points.append(point)
 
-points = np.stack((x, y), axis=1)
-points = list(points)
-dist = 10
-
-c = np.array(calc_curve(points, dist)).astype(np.int)
-
-plt.plot(c[0][:,0], c[0][:,1], 'o', lw=0.1, ms=3, c='r')
-plt.plot(x, y, 'o', lw=0.1, ms=2, c='b')
-plt.show()
-
-# import matplotlib.pyplot as plt
-# import numpy as np
-# from geomdl import fitting
-# from geomdl.visualization import VisMPL as vis
-#
-# points = np.stack((x, y), axis=1)
-# degree = 2
-# curve = fitting.interpolate_curve(list(points), degree)
-# curve.delta = 0.01
-# curve.vis = vis.VisCurve2D(ctrlpts=False, legend=False, axes=False, figure_size=[8/3, 8/3])
-# curve.render(save='out.png')
-#
-# plt.plot(x, y, 'o', lw=0.2, ms=2)
-# plt.ylim(np.mean(y) - 32, np.mean(y) + 32)
-# plt.xlim(np.mean(x) - 32, np.mean(x) + 32)
-# plt.show()
+    points = np.array(points)
+    plt.figure(i)
+    plt.subplot(121)
+    plt.plot(x, y, 'o', lw=0.1, ms=2, c='b')
+    plt.subplot(122)
+    plt.plot(points[:, 0], points[:, 1], 'o', lw=0.5, ms=2, c='r')
+    plt.savefig(os.path.join('simplify_clusters_dist14', '{0}.png'.format(i)))
 
 
